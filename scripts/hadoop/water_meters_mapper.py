b425cd299dfd4f6d9b1044a0d483d1be2385555e
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2021-01-19 09:11:37 +0200

    retrieve readings as bulk

--- scripts/hadoop/water_meters_mapper.py
+++ scripts/hadoop/water_meters_mapper.py
@@ -1,24 +1,29 @@
 #!/usr/local/bin/python
+import math
 import sys
-import requests
-import uuid
 
-READING_URL = 'https://gisservices.surrey.ca/arcgis/rest/services/Public/Water/MapServer/find?searchText={id}&contains=true&searchFields=FACILITYID&sr=&layers=1&f=pjson'
+import requests
 
 FACILITYID, HOUSE_NO, STREET_NO, ACCOUNT_NO, FOLIO, STATUS, GPS, IMAGE, LOTLINK, LATITUDE, LONGITUDE = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
+DOWNLOAD_URL = 'https://gisservices.surrey.ca/arcgis/rest/services/Public/Water/MapServer/find?searchText=DISPLACEMENT&contains=true&searchFields=TYPE_A&sr=&layers=1&returnGeometry=true&returnZ=false&returnM=falsereturnUnformattedValues=true&returnFieldName=true&f=pjson'
 
-def get_reading(facility_id):
-    url = READING_URL.format(id=facility_id)
-    resp = requests.get(url)
+def get_reading():
+    readings = {}
+    resp = requests.get(DOWNLOAD_URL)
     if resp.status_code != 200:
-        raise RuntimeError('GET {} {}'.format(url, resp.status_code))
-    meter_attributes = resp.json()['results'][0]['attributes']
-    return [meter_attributes['LAST READ'], meter_attributes['LAST READ DATE']]
+        raise RuntimeError('GET {} {}'.format(DOWNLOAD_URL, resp.status_code))
+    for result in resp.json()['results']:
+        a = result['attributes']
+        readings[a['FACILITYID']] = [a['LAST_READING_A'], a['LAST_READING_DATE_A']]
+    return readings
 
+readings_dic = get_reading()
 
 def map_line(line_fields):
-    readings = get_reading(line_fields[FACILITYID])
+    readings = readings_dic[line_fields[FACILITYID]]
+    if readings is None:
+        return None
     mapped = ["-".join([line_fields[FACILITYID], readings[1]])]
     mapped.extend(line_fields[FACILITYID:FOLIO])
     mapped.extend(readings)
@@ -30,8 +35,16 @@ def map_line(line_fields):
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    if line.startswith('FACILITYID'):
+        continue
     # split the line into words
     fields = line.split(",")
+    if fields[STATUS] != 'In Service':
+        continue
     # increase counters
-    new_line = map_line(fields)
-    print(new_line)
+    try:
+        new_line = map_line(fields)
+        if map_line != None:
+            print(new_line)
+    except:
+        continue
